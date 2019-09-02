200a9522894946c8ae9ea00f8e57554b54d9ab8c
daniarmando <daniel.armando@live.com>
2019-09-02 09:12:01 -0300

    Mostrando total de páginas dinamicamente

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 //import org.slf4j.Logger;
@@ -72,9 +73,9 @@ public class CervejasController {
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("origens", Origem.values());
 		
-		System.out.println(">>> pageNumber: " + pageable.getPageNumber());
-		
-		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable));
+		Page<Cerveja> pagina = cervejas.filtrar(cervejaFilter, pageable);
+		mv.addObject("pagina", pagina);
+								
 		return mv;
 	}
 }
