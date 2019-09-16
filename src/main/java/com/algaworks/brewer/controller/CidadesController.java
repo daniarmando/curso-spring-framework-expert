8aacb2b89612a64d971967fd94227445e834e570
daniarmando <daniel.armando@live.com>
2019-09-16 10:47:30 -0300

    Selecionado cidades pelo estado com Ajax

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -1,25 +1,35 @@
 package com.algaworks.brewer.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
 	
-	@RequestMapping("/cidades/novo")
+	@Autowired
+	private Cidades cidades;
+	
+	@RequestMapping("/novo")
 	public String novo(Cidade cidade) {
 		return "cidade/CadastroCidade";
 	}
 	
-	@RequestMapping(value = "/cidades/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cidade cidade, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			model.addAttribute(cidade);
@@ -30,5 +40,10 @@ public class CidadesController {
 		return "redirect:/cidades/novo";
 	}
 	
+	@RequestMapping(consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
+			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {		
+		return cidades.findByEstadoCodigo(codigoEstado);
+	}
 
 }
