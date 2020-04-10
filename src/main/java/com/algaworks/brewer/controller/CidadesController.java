84bd5d9b9ca41429af762bd3e9cfe11af19a297d
daniarmando <daniel.armando@live.com>
2020-04-10 11:06:21 -0300

    Desafio: Finalizar cadastro de cidade

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -5,16 +5,21 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cidade;
 import com.algaworks.brewer.repository.Cidades;
+import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.CadastroCidadeService;
+import com.algaworks.brewer.service.exception.NomeCidadeJaCadastradaException;
 
 @Controller
 @RequestMapping("/cidades")
@@ -23,25 +28,43 @@ public class CidadesController {
 	@Autowired
 	private Cidades cidades;
 	
-	@RequestMapping("/novo")
-	public String novo(Cidade cidade) {
-		return "cidade/CadastroCidade";
+	@Autowired
+	private Estados estados;
+	
+	@Autowired 
+	private CadastroCidadeService cadastroCidadeService;
+	
+	@RequestMapping("/nova")
+	public ModelAndView nova(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("cidade/CadastroCidade");
+		mv.addObject("estados", estados.findAll());
+		return mv;
+	}	
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
+			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) { }
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 	
-	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
-		if (result.hasErrors()) {			
-			return novo(cidade);
+	@PostMapping("/nova")
+	public ModelAndView salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return nova(cidade);
 		}
 		
-		attributes.addFlashAttribute("mensagem", "Cidade cadastrada com sucesso.");
-		return "redirect:/cidades/novo";
-	}
-	
-	@RequestMapping(consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
-			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {		
-		return cidades.findByEstadoCodigo(codigoEstado);
+		try {
+			cadastroCidadeService.salvar(cidade);
+		} catch (NomeCidadeJaCadastradaException e ) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
+			return nova(cidade);
+		}
+					
+		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso.");
+		return new ModelAndView("redirect:/cidades/nova");
 	}
 
 }
