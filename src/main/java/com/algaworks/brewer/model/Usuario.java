0d95d03b81e1601dd4a288740df4f5dc9efc8ccd
daniarmando <daniel.armando@live.com>
2020-04-18 11:06:13 -0300

    Validador customizado para confirmação de senha

--- src/main/java/com/algaworks/brewer/model/Usuario.java
+++ src/main/java/com/algaworks/brewer/model/Usuario.java
@@ -13,11 +13,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.algaworks.brewer.validation.AtributoConfirmacao;
+
+@AtributoConfirmacao(atributo = "senha", atributoConfirmacao = "confirmacaoSenha", message = "Confirmação da senha não confere")
 @Entity
 @Table(name = "usuario")
 public class Usuario implements Serializable {
@@ -34,9 +38,12 @@ public class Usuario implements Serializable {
 	@NotBlank(message = "E-mail é obrigatório")
 	@Email(message = "E-mail inválido")
 	private String email;
-	
+		
 	private String senha;
 	
+	@Transient
+	private String confirmacaoSenha;
+		
 	private Boolean ativo;
 	
 	@NotNull(message = "Selecione pelo menos um grupo")
@@ -71,6 +78,12 @@ public class Usuario implements Serializable {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
 	}
 	public Boolean getAtivo() {
 		return ativo;
