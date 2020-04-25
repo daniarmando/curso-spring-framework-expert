e9594b14668675e662d20137cacfd638ca26ec3e
daniarmando <daniel.armando@live.com>
2020-04-25 11:24:13 -0300

    Autenticando usuário e senha no banco de dados

--- src/main/java/com/algaworks/brewer/repository/Usuarios.java
+++ src/main/java/com/algaworks/brewer/repository/Usuarios.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.helper.usuario.UsuariosQueries;
 import com.google.common.base.Optional;
 
 @Repository
-public interface Usuarios extends JpaRepository<Usuario, Long> {
+public interface Usuarios extends JpaRepository<Usuario, Long>, UsuariosQueries {
 	
 	Optional<Usuario> findByEmail(String email);
 
---
+++ src/main/resources/db/migration/V10__inserir_usuario_administrador.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (nome, email, senha, ativo) 
+	VALUES ('Admin', 'admin@brewer.com', '$2a$10$eAaz5dgIQ189n6skMbnmvuTgqHLhxaR6ChT3javXDcOWPy.zQKzma', 1);
