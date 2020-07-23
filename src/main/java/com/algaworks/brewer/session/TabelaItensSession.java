44897b865876a94eb67281d20dd5729456ac1330
daniarmando <daniel.armando@live.com>
2020-07-22 21:23:23 -0300

    Simulando um escopo de view

---
+++ src/main/java/com/algaworks/brewer/session/TabelaItensSession.java
@@ -0,0 +1,46 @@
+package com.algaworks.brewer.session;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.ItemVenda;
+
+@SessionScope
+@Component
+public class TabelaItensSession {
+	
+	private Set<TabelaItensVenda> tabelas = new HashSet<>();
+
+	public void adicionarItemCerveja(String uuid, Cerveja cerveja, int quantidade) {
+		TabelaItensVenda tabela = buscarTabelaPorId(uuid);
+		tabela.adicionarItemCerveja(cerveja, quantidade);		
+		tabelas.add(tabela);
+	}
+
+	public void alterarQuantidadeItens(String uuid, Cerveja cerveja, Integer quantidade) {
+		TabelaItensVenda tabela = buscarTabelaPorId(uuid);
+		tabela.alterarQuantidadeItens(cerveja, quantidade);		
+	}
+
+	public void excluirItem(String uuid, Cerveja cerveja) {
+		TabelaItensVenda tabela = buscarTabelaPorId(uuid);
+		tabela.excluirItem(cerveja);
+	}
+
+	public List<ItemVenda> getItens(String uuid) {
+		return buscarTabelaPorId(uuid).getItens();
+	}
+	
+	private TabelaItensVenda buscarTabelaPorId(String uuid) {
+		TabelaItensVenda tabela = tabelas.stream()
+				.filter(t -> t.getUuid().equals(uuid))
+				.findAny().orElse(new TabelaItensVenda(uuid));
+		return tabela;
+	}
+
+}
