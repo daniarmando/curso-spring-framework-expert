8de80a9e8b149a125b4367e9b54af17e14908923
daniarmando <daniel.armando@live.com>
2020-07-27 06:12:40 -0300

    Desafio: Criando mapeamento para tabelas de venda

--- src/main/java/com/algaworks/brewer/model/ItemVenda.java
+++ src/main/java/com/algaworks/brewer/model/ItemVenda.java
@@ -2,12 +2,35 @@ package com.algaworks.brewer.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item_venda")
 public class ItemVenda {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+		
 	private Integer quantidade;
+	
+	@Column(name = "valor_unitario")
 	private BigDecimal valorUnitario;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_cerveja")
 	private Cerveja cerveja;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_venda")
+	private Venda venda;
 	
 	public Long getCodigo() {
 		return codigo;
@@ -38,13 +61,22 @@ public class ItemVenda {
 		return valorUnitario.multiply(new BigDecimal(quantidade));
 	}
 	
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cerveja == null) ? 0 : cerveja.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -54,12 +86,12 @@ public class ItemVenda {
 		if (getClass() != obj.getClass())
 			return false;
 		ItemVenda other = (ItemVenda) obj;
-		if (cerveja == null) {
-			if (other.cerveja != null)
+		if (codigo == null) {
+			if (other.codigo != null)
 				return false;
-		} else if (!cerveja.equals(other.cerveja))
+		} else if (!codigo.equals(other.codigo))
 			return false;
 		return true;
-	}	
-
+	}
+		
 }
