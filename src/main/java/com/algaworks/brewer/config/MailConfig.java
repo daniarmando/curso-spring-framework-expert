0ce429f2a612d143bfa642ca15fa8a36e788832a
daniarmando <daniel.armando@live.com>
2020-07-31 06:52:26 -0300

    Configuração do servidor de e-mail com arquivos externos

---
+++ src/main/java/com/algaworks/brewer/config/MailConfig.java
@@ -0,0 +1,42 @@
+package com.algaworks.brewer.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@PropertySource({ "classpath:env/mail-${ambiente:local}.properties" })
+@PropertySource(value = { "file://${HOME}/.brewer-mail.properties" }, ignoreResourceNotFound = true) // se for carregar por arquivo externo como /home/daniel
+public class MailConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public JavaMailSender mailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.sendgrid.net");
+		mailSender.setPort(587);
+		//mailSender.setUsername("daniel.armandobr@gmail.com");
+		mailSender.setUsername(env.getProperty("mail.username"));
+		mailSender.setPassword(env.getProperty("mail.password"));			
+		
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.starttls.enable", true);
+		props.put("mail.debug", false);
+		props.put("mail.smtp.connectiontimeout", 10000); //milisegundos
+		
+		mailSender.setJavaMailProperties(props);
+		
+		return mailSender;
+	}
+
+}
