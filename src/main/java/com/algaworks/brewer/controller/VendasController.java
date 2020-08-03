2fd1fd8167464c444e4343b99e0045b1e29bfb6f
daniarmando <daniel.armando@live.com>
2020-08-03 05:50:38 -0300

    Configuração de chamadas assíncronas

--- src/main/java/com/algaworks/brewer/controller/VendasController.java
+++ src/main/java/com/algaworks/brewer/controller/VendasController.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.controller.validator.VendaValidator;
+import com.algaworks.brewer.mail.Mailer;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.StatusVenda;
 import com.algaworks.brewer.model.TipoPessoa;
@@ -50,6 +51,9 @@ public class VendasController {
 	
 	@Autowired
 	private Vendas vendas;
+	
+	@Autowired
+	private Mailer mailer;
 	
 	@InitBinder("venda")
 	public void inicializarValidador(WebDataBinder binder) {
@@ -111,6 +115,7 @@ public class VendasController {
 		}			
 		
 		venda.setUsuario(usuarioSistema.getUsuario());
+		mailer.enviar();
 				
 		cadastroVendaService.salvar(venda);
 		attributes.addFlashAttribute("mensagem", "Venda salva e e-mail enviado com sucesso");
---
+++ src/main/java/com/algaworks/brewer/mail/Mailer.java
@@ -0,0 +1,21 @@
+package com.algaworks.brewer.mail;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mailer {
+
+	@Async
+	public void enviar() {
+		try {
+		System.out.println(">>>> Enviando e-mail");
+		Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(">>>> e-mail enviado");
+	}
+	
+}
